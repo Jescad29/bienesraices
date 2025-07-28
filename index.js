1da275e804dd095659da9d46ddd4be746957f955
Chuy <jescad27@gmail.com>
2025-07-27 23:02:15 -0600

    chore: Se finaliza la configuracion del servidor y se agrega un archivo para notas

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("hola mundo")
+import express from 'express';
+import usuarioRoutes from './routes/usuario.routes.js'
+
+const app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+app.use('/auth', usuarioRoutes);
+app.use('/login', usuarioRoutes);
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+});
+ 
+// Notas:
+
+// import express from 'express';
+// import usuarioRoutes from './routes/usuario.routes.js'
+
+// const app = express();
+
+// Indica que queremos utilizar pug como view engine y que carpeta es la de views (En donde vamos a utilizar nuestras vistas)
+// ('set' es para la configuración)
+// app.set('view engine', 'pug');
+// app.set('views', './views');
+
+//  Routing (use es para las vistas.)
+// app.use('/auth', usuarioRoutes)
+
+// Definir un proyecto y arrancar el proyecto
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(`El servidor esta funcionando en el puerto ${port}`)
+// });
+
+
---
+++ routes/usuario.routes.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('auth/login', {
+        autenticado:true 
+    })
+});
+
+
+
+export default router
+
+// Notas:
+
+//Creamos las rutas con router, las cuales importaremos en nuestro archivo principal index.js.
+
+// router.get('/', function (req, res) {
+//     res.json({msg: 'Hola mundo en express'})
+// });
+
+//  - Solo podemos tener una ruta, pero asignarle diferentes metodos.
+
+//  - const router = express.Router(); Se crea la instancia de express.Router()
+
+//  - Para agruparlas en un solo grupo de rutas.
+//  router.route('/')
+//         .get(function (req, res) {
+//         res.json({ msg: 'Respuesta de tipo get' });
+//         })
+//         .post(function (req, res) {
+//             res.json({msg: 'Respuesta de tipo post'})
+//         })
+
+// - "Render"(res.render('')) nos ayuda a renderizar las vistas, ya previamente indicamos con set cual seria el view engine que en este caso es pug y la carpeta en la que se encuentran "./views" lo cual tambien nos ayuda a que ya no tengamos que indicarle la carpeta ./views:
+
+// router.get('/', function (req, res) {
+//     res.render('auth/login')
+// });
